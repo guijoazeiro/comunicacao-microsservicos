29d83485494f4a461e81d492eb75611e6469e79d
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-04-11 09:36:40 -0300

    feat: add endpoint security

--- auth-api/src/modules/user/service/UserService.js
+++ auth-api/src/modules/user/service/UserService.js
@@ -11,9 +11,11 @@ class UserService {
     async findByEmail(req) {
         try {
             const { email } = req.params;
+            const { authUser } = req;
             this.validateRequestData(email);
             let user = await UserRepository.findByEmail(email);
             this.validateUserNotFound(user);
+            this.validateAuthenticatedUser(user, authUser);
             return {
                 status: httpStatus.SUCCESS,
                 user: {
@@ -39,6 +41,13 @@ class UserService {
             throw new UserException(httpStatus.BAD_REQUEST, 'User was not found.');
         }
     }
+
+    validateAuthenticatedUser(user, authUser) {
+        if (!authUser || user.id !== authUser.id) {
+            throw new UserException(httpStatus.FORBIDDEN, 'You cannot see this user data.');
+        }
+    }
+
     async getAccessToken(req) {
         try {
             const { email, password } = req.body;
