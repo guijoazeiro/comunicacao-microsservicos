81d69e0f53a95592279705f061bb87351d78b300
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-04-11 09:06:36 -0300

    chore: add checkToken middleware

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import * as db from './src/config/initialData.js';
 import userRoutes from './src/modules/user/routes/UserRoutes.js';
+import checkToken from "./src/config/auth/checkToken.js";
+
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
@@ -10,6 +12,8 @@ db.createInitialData();
 app.use(express.json());
 
 app.use(userRoutes);
+
+app.use(checkToken);
 
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
