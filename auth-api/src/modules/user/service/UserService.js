c794c2d617a6c03a327e455a1f8acb0be5766205
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-04-11 08:53:35 -0300

    chore: add accessToken route

--- auth-api/src/modules/user/service/UserService.js
+++ auth-api/src/modules/user/service/UserService.js
@@ -1,6 +1,10 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
+
 import UserRepository from '../repository/UserRepository.js';
 import UserException from '../exception/UserException.js';
 import * as httpStatus from '../../../config/constants/httpStattus.js';
+import * as secrets from '../../../config/constants/secrets.js';
 
 
 class UserService {
@@ -34,7 +38,39 @@ class UserService {
         if (!user) {
             throw new UserException(httpStatus.BAD_REQUEST, 'User was not found.');
         }
-    }    
+    }
+    async getAccessToken(req) {
+        try {
+            const { email, password } = req.body;
+            this.validateAccessTokenData(email, password);
+            let user = await UserRepository.findByEmail(email);
+            this.validateUserNotFound(user);
+            await this.validatePassword(password, user.password);
+            const authUser = { id: user.id, name: user.name, email: user.email };
+            const accessToken = jwt.sign({ authUser }, secrets.API_SECRET, { expiresIn: '1d' });
+            return {
+                status: httpStatus.SUCCESS,
+                accessToken,
+            }
+        } catch (error) {
+            return {
+                status: error.status ? error.status : httpStatus.INTERNAL_SERVER_ERROR,
+                message: error.message,
+            }
+        }
+    }
+
+    validateAccessTokenData(email, password) {
+        if (!email || !password) {
+            throw new UserException(httpStatus.UNAUTHORIZED, 'Email and password must be informed.');
+        }
+    }
+
+    async validatePassword(password, hashPassword) {
+        if (!(await bcrypt.compareSync(password, hashPassword))) {
+            throw new UserException(httpStatus.UNAUTHORIZED, "Password doesn't match.");
+        }
+    }
 }
 
 export default new UserService();
