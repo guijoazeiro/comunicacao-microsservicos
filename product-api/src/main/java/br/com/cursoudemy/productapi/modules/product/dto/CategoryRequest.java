33e9a8b05b303fbfcc35bef07e024f10663201cb
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-04-18 17:01:39 -0300

    chore: add post category route in product-api

---
+++ product-api/src/main/java/br/com/cursoudemy/productapi/modules/product/dto/CategoryRequest.java
@@ -0,0 +1,8 @@
+package br.com.cursoudemy.productapi.modules.product.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryRequest {
+    private String description;
+}
---
+++ product-api/src/main/java/br/com/cursoudemy/productapi/modules/product/repository/SupplierRepository.java
@@ -0,0 +1,7 @@
+package br.com.cursoudemy.productapi.modules.product.repository;
+
+import br.com.cursoudemy.productapi.modules.product.model.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+}
