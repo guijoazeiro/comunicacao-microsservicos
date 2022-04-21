b22db9034ea7f7708388aab9d63e76436187f6a6
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-04-21 17:28:24 -0300

    chore: add get methods to product route in product-api

--- product-api/src/main/java/br/com/cursoudemy/productapi/modules/product/service/ProductService.java
+++ product-api/src/main/java/br/com/cursoudemy/productapi/modules/product/service/ProductService.java
@@ -10,6 +10,9 @@ import br.com.cursoudemy.productapi.modules.supplier.service.SupplierService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Service
@@ -51,5 +54,71 @@ public class ProductService {
         if (isEmpty(request.getSupplierId())) {
             throw new ValidationException("The supplier ID was not informed.");
         }
+    }
+
+    public List<ProductResponse> findAll() {
+        return productRepository
+                .findAll()
+                .stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public List<ProductResponse> findByName(String name) {
+        if (isEmpty(name)) {
+            throw new ValidationException("The product name must be informed.");
+        }
+        return productRepository
+                .findByNameIgnoreCaseContaining(name)
+                .stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public List<ProductResponse> findBySupplierId(Integer supplierId) {
+        if (isEmpty(supplierId)) {
+            throw new ValidationException("The product' supplier ID name must be informed.");
+        }
+        return productRepository
+                .findBySupplierId(supplierId)
+                .stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public List<ProductResponse> findByCategoryId(Integer categoryId) {
+        if (isEmpty(categoryId)) {
+            throw new ValidationException("The product' category ID name must be informed.");
+        }
+        return productRepository
+                .findByCategoryId(categoryId)
+                .stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public ProductResponse findByIdResponse(Integer id) {
+        return ProductResponse.of(findById(id));
+    }
+
+    public Product findById(Integer id) {
+        validateInformedId(id);
+        return productRepository
+                .findById(id)
+                .orElseThrow(() -> new ValidationException("There's no product for the given ID."));
+    }
+
+    private void validateInformedId(Integer id) {
+        if (isEmpty(id)) {
+            throw new ValidationException("The supplier ID must be informed.");
+        }
+    }
+
+    public Boolean existsByCategoryId(Integer categoryId) {
+        return productRepository.existsByCategoryId(categoryId);
+    }
+
+    public Boolean existsBySupplierId(Integer categoryId) {
+        return productRepository.existsByCategoryId(categoryId);
     }
 }
