b1a69aa2d3dc582cba5ed3e987061ce15e15e26c
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-04-21 17:03:45 -0300

    chore: add get methods to product-api

--- product-api/src/main/java/br/com/cursoudemy/productapi/modules/category/repository/CategoryRepository.java
+++ product-api/src/main/java/br/com/cursoudemy/productapi/modules/category/repository/CategoryRepository.java
@@ -3,6 +3,8 @@ package br.com.cursoudemy.productapi.modules.category.repository;
 import br.com.cursoudemy.productapi.modules.category.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
+import java.util.List;
 
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    List<Category> findByDescriptionIgnoreCaseContaining(String description);
 }
