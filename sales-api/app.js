cf186272e34bef394da05378313f3c3bfb9d1431
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-04-29 16:40:02 -0300

    chore: add initialData file to sales-api

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
 import { connectMongoDb } from './src/config/db/mongoDbConfig.js';
-
+import { createInitialData } from './src/config/db/initialData.js';
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
 
 connectMongoDb();
+createInitialData();
 
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
