73ab136da924eabd1524e20c971ac43a716727cd
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-04-29 16:50:41 -0300

    chore: add jwt interceptor to sales-api

--- sales-api/src/config/secrets/secrets.js
+++ sales-api/src/config/secrets/secrets.js
@@ -3,3 +3,8 @@ const env = process.env;
 export const MONGO_DB_URL = env.MONGO_DB_URL
     ? env.MONGO_DB_URL
     : "mongodb://localhost:27017"
+
+
+export const API_SECRET = env.API_SECRET
+    ? env.API_SECRET
+    : "YXV0aC1hcGktc2VjcmV0LWRldi0xMjM0NTY=";    
