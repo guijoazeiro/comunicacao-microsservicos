5d3fe6af8610f2c0c28933da2e073789da09cbe9
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-04-29 15:59:00 -0300

    chore: add mongodb config

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 
+import { connectMongoDb } from './src/config/db/mongoDbConfig.js';
+
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
+
+connectMongoDb();
 
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
---
+++ sales-api/src/config/db/mongoDbConfig.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+import { MONGO_DB_URL } from "../secrets/secrets.js";
+export function connectMongoDb() {
+    mongoose.connect(MONGO_DB_URL, {
+        useNewUrlParser: true,
+        serverSelectionTimeoutMS: 180000,
+    });
+    mongoose.connection.on("connected", function () {
+        console.info("The application connected to MongoDB successfully!");
+    });
+    mongoose.connection.on("error", function () {
+        console.error("The application connected to MongoDB successfully!");
+    });
+}
