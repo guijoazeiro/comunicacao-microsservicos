1bb4aa462087a0d0409aa050130c555e4761494b
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-05-02 16:25:14 -0300

    chore: add sales-api sender

--- sales-api/src/modules/product/rabbitmq/productStockUpdateSender.js
+++ sales-api/src/modules/product/rabbitmq/productStockUpdateSender.js
@@ -1,10 +1,10 @@
 import amqp from "amqplib/callback_api.js";
 
-import { RABBIT_MQ_URL } from "../../../../config/constants/secrets.js";
+import { RABBIT_MQ_URL } from "../../../config/constants/secrets.js";
 import {
   PRODUCT_TOPIC,
   PRODUCT_STOCK_UPDATE_ROUTING_KEY,
-} from "../../../../config/rabbitmq/queue.js";
+} from "../../../config/rabbitmq/queue.js";
 
 export function sendMessageToProductStockUpdateQueue(message) {
   amqp.connect(RABBIT_MQ_URL, (error, connection) => {
