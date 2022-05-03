7a21bb8350f0d6135bb4e873baf1c8304b9fc72c
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-05-03 15:07:19 -0300

    chore: add updateOrder to sales-api

--- sales-api/src/modules/sales/service/OrderService.js
+++ sales-api/src/modules/sales/service/OrderService.js
@@ -2,6 +2,8 @@ import OrderRepository from "../repository/OrderRepository.js";
 import { sendMessageToProductStockUpdateQueue } from "../../product/rabbitmq/productStockUpdateSender.js";
 import { PENDING, ACCEPTED, REJECTED } from "../status/OrderStatus.js";
 
+import OrderException from '../../sales/exception/OrderException.js';
+
 import { BAD_REQUEST, SUCCESS, INTERNAL_SERVER_ERROR, } from "../../../config/constants/httpStatus.js";
 
 class OrderService {
@@ -17,6 +19,7 @@ class OrderService {
                 updateAt: new Date(),
                 products: orderData
             };
+            await this.validateProductStock(order, authorization);
             let createOrder = await OrderRepository.save(order);
             sendMessageToProductStockUpdateQueue(createOrder.products);
             return {
@@ -33,6 +36,35 @@ class OrderService {
     validateOrderData(data) {
         if (!data || !data.products) {
             throw new OrderException(BAD_REQUEST, "The products must be informed.");
+        }
+    }
+
+    async updateOrder(orderMessage) {
+        try {
+            const order = JSON.parse(orderMessage);
+            if (order.salesId && order.status) {
+                let existingOrder = await OrderRepository.findById(order.salesId);
+                if (existingOrder && order.status !== existingOrder.status) {
+                    existingOrder.status = order.status;
+                    existingOrder.updatedAt = new Date();
+                    await OrderRepository.save(existingOrder);
+                }
+            } else {
+                console.warn("The order message was not complete.");
+            }
+        } catch (err) {
+            console.error("Could not parse order message from queue.");
+            console.error(err.message);
+        }
+    }
+
+    async validateProductStock(order, token) {
+        let stockIsOk = await ProductClient.checkProducStock(order, token);
+        if (!stockIsOk) {
+            throw new OrderException(
+                BAD_REQUEST,
+                "The stock is out for the products."
+            );
         }
     }
 }
