578941df3c477bee14cc24442d685d61621139f1
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-05-04 14:03:34 -0300

    chore: add sales-api controller

--- sales-api/app.js
+++ sales-api/app.js
@@ -4,6 +4,9 @@ import { connectMongoDb } from './src/config/db/mongoDbConfig.js';
 import { createInitialData } from './src/config/db/initialData.js';
 import { connectRabbitMq } from './src/config/rabbitmq/rabbitConfig.js'
 
+import orderRoutes from './src/modules/sales/routes/OrderRoutes.js';
+import checkToken from './src/config/auth/checkToken.js';
+
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
@@ -11,6 +14,9 @@ const PORT = env.PORT || 8082;
 connectMongoDb();
 createInitialData();
 connectRabbitMq()
+
+app.use(checkToken);
+app.use(orderRoutes);
 
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
